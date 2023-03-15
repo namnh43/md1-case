ccf5c9d2b3087c38bef6c23d214ce68b64a741da
namnh <33839115+namnh43@users.noreply.github.com>
2023-03-15 14:06:33 +0700

    update before change silder

--- js/local-storage.js
+++ js/local-storage.js
@@ -1,5 +1,5 @@
 function setContent(page){
     // alert(page)
-    window.localStorage.setItem('page', page);
+    window.sessionStorage.setItem('page', page);
     window.location.href = '../index.html'
 }
