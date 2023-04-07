e7143649bf5bf580faa471bea4351cd993af71f5
namnh <33839115+namnh43@users.noreply.github.com>
2023-04-07 14:52:29 +0700

    Update bubbler-script.js

--- src/js/bubbler-script.js
+++ src/js/bubbler-script.js
@@ -2,12 +2,9 @@ var options =
     [
         {
             icon:'<img src="src/img/icon/telephone.png" width="55px" height="55px">',
-            label: '0988.123.456',
+            label: '(+84)338856789',
             display: {
                 // padding: '5px',
-            },
-            callback: function() {
-                alert('call to open zalo');
             }
         },
         {
